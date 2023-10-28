83b8b7bb2d37a8d683c07ac7456d26ac823d21d4
kdlibiran <kglibiran@up.edu.ph>
2023-10-29 00:29:05 +0800

    Topbar is fixed

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export const LeftSidebar = () => {
   return (
-    <section className="fixed left-0 flex-col md:w-64 h-full w-30 sm:flex hidden pt-5 justify-center items-center">
-      <div className="flex w-full flex-1 flex-col gap-10 px-6 pt-2">
+    <section className="fixed top-0 left-0 flex-col md:w-64 h-full w-30 sm:flex hidden pt-24 justify-center items-center">
+      <div className="flex w-full flex-1 flex-col gap-10 px-6">
         {sidebarLinks.map((link) => (
           <Link
             href={link.route}
