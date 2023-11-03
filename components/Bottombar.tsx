73df40ebb7ddf10bdf98c19f8ab76bc0f549535a
kdlibiran <kglibiran@up.edu.ph>
2023-11-03 22:31:54 +0800

    Fixed mobile view spacing

--- components/Bottombar.tsx
+++ components/Bottombar.tsx
@@ -4,7 +4,7 @@ import { sidebarLinks } from "@/constants/index";
 export default function Bottombar() {
   return (
     <section className="w-screen h-16 bottom-0 left-0 fixed flex row sm:hidden items-center justify-center bg-background">
-      <div className="flex w-full flex-1 flex-row gap-14 items-center justify-center">
+      <div className="flex w-full flex-1 flex-row gap-10 items-center justify-center">
         {sidebarLinks.map((link) => (
           <Link
             href={link.route}
